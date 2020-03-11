85f8802f1f6971bb04c6c9286f0bf79ef6ae6225
simona <palumbosimona118@gmail.com>
2020-03-11 10:04:41 +0100

    up

---
+++ shop/Detergente.php
@@ -0,0 +1,33 @@
+<?php
+
+
+require_once __DIR__ . '/../traits/GetPosition.php';
+
+class Detergente {
+  public $categoria;
+  public $marca;
+  public $prezzo;
+  public $formato;
+  public $inci;
+
+
+  function __construct($_categoria, $_formato, $_prezzo, $_marca) {
+    $this->categoria = $_categoria;
+    $this->formato = $_formato;
+    $this->prezzo = $_prezzo;
+    $this->marca = $_marca;
+  }
+
+}
+
+$lineacars = new Detergente('shampoo', '500ml', 35, 'powershell');
+$lineacars-> = 'shampooauto';
+
+
+$internoauto = new Product('detergente', '200ml', 20, 'powershell');
+$internoauto-> = 'lavatappezzeria';
+
+
+
+
+ ?>
---
+++ shop/index.php
@@ -0,0 +1,10 @@
+<?php
+require_once 'Detergente.php';
+
+$lineacars = new Detergente('shampoo', '500ml', 35, 'powershell');
+//var_damp($lineacars);
+$lineacars->categoria = 'shampooauto';
+
+$internoauto = new Detergente('pasta', '200ml', 20, 'powershell');
+$internoauto-> = 'lavatappezzeria';
+?>
